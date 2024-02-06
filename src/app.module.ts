d5290c250339b95ac0a233cc521745824d5a88c4
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2024-02-06 18:48:20 +0300

    In progress of configuring PostgreSQL and Swagger on a Linux machine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PromoModule } from './promo/promo.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    PromoModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   // Обращаемся к объекту NestFactory (сразу видно объектно-ориентированный паттерн!) и инстанцируем наше приложение
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Sample Project')
+    .setDescription(
+      `[The source API definition (json)](http://${process.env.SERVER}:${process.env.PORT}/api-json)`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   // Читаем из файла .env наш сервер и порт
   const port: number = parseInt(process.env.PORT);
   const server: string = process.env.SERVER;
